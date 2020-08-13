87bc365576c39461aca148f437df335d7c666947
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-13 20:39:14 -0300

    test: ensure SignUpController calls Validator with correct values

---
+++ api/src/presentation/controllers/signup/signup-controller.spec.ts
@@ -0,0 +1,47 @@
+import { SignUpController, SignUpModel } from './signup-controller'
+import { Validator, HttpRequest } from '../../protocols'
+
+const makeFakeRequest = (): HttpRequest<SignUpModel> => ({
+  body: {
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password'
+  }
+})
+
+const makeValidator = (): Validator => {
+  class ValidatorStub implements Validator {
+    public validate (_input: any): Error {
+      return null
+    }
+  }
+
+  return new ValidatorStub()
+}
+
+interface SutTypes {
+  validatorStub: Validator
+  sut: SignUpController
+}
+
+const makeSut = (): SutTypes => {
+  const validatorStub = makeValidator()
+  const sut = new SignUpController(validatorStub)
+
+  return {
+    validatorStub,
+    sut
+  }
+}
+
+describe('SignUp Controller', () => {
+  test('Should call Validator with correct values', async () => {
+    const { sut, validatorStub } = makeSut()
+    const validateSpy = spyOn(validatorStub, 'validate')
+
+    const httpRequest = makeFakeRequest()
+    await sut.handle(httpRequest)
+
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+})
