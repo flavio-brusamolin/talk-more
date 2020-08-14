6786ca2030993b7cc6a58457d64027dda96ae94a
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-14 11:47:06 -0300

    feat: ensure UserMongoRepository returns a user on loadByEmail success

--- api/src/infra/db/mongodb/user/user-mongo-repository.ts
+++ api/src/infra/db/mongodb/user/user-mongo-repository.ts
@@ -1,9 +1,17 @@
-import { AddUserRepository } from '../../../../data/protocols'
+import { AddUserRepository, LoadUserByEmailRepository } from '../../../../data/protocols'
 import { AddUserModel } from '../../../../domain/use-cases/add-user'
 import { User } from '../../../../domain/models/user'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class UserMongoRepository implements AddUserRepository {
+export class UserMongoRepository implements AddUserRepository, LoadUserByEmailRepository {
+  public async loadByEmail (email: string): Promise<User> {
+    const userCollection = await MongoHelper.getCollection('users')
+
+    const userRecord = await userCollection.findOne({ email })
+
+    return userRecord && MongoHelper.map(userRecord)
+  }
+
   public async add (userData: AddUserModel): Promise<User> {
     const userCollection = await MongoHelper.getCollection('users')
 
