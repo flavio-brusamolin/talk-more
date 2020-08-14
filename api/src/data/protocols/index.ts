f429f932385717f89668c2de97ad2f17965d6e50
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-14 09:27:01 -0300

    feat: ensure DbAuthenticateUser calls HashComparator with correct values

--- api/src/data/protocols/index.ts
+++ api/src/data/protocols/index.ts
@@ -1,3 +1,4 @@
 export * from './criptography/hasher'
+export * from './criptography/hash-comparator'
 export * from './db/add-user-repository'
 export * from './db/load-user-by-email-repository'
--- api/src/data/use-cases/authenticate-user/db-authenticate-user.ts
+++ api/src/data/use-cases/authenticate-user/db-authenticate-user.ts
@@ -1,12 +1,18 @@
 import { AuthenticateUser, AuthenticateUserModel } from '../../../domain/use-cases/authenticate-user'
-import { LoadUserByEmailRepository } from '../../protocols'
+import { LoadUserByEmailRepository, HashComparator } from '../../protocols'
 
 export class DbAuthenticateUser implements AuthenticateUser {
-  public constructor (private readonly loadUserByEmailRepository: LoadUserByEmailRepository) {}
+  public constructor (
+    private readonly loadUserByEmailRepository: LoadUserByEmailRepository,
+    private readonly hashComparator: HashComparator
+  ) {}
 
   public async authenticate (credentials: AuthenticateUserModel): Promise<string> {
-    await this.loadUserByEmailRepository.loadByEmail(credentials.email)
+    const user = await this.loadUserByEmailRepository.loadByEmail(credentials.email)
+    if (!user) {
+      return null
+    }
 
-    return null
+    await this.hashComparator.compare(credentials.password, user.password)
   }
 }
