96825d93e45a2ad334b69ee46a1b436bdc0a530a
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-14 14:32:35 -0300

    feat: ensure BCryptAdapter returns a valid hash on bcrypt hash success

--- api/src/infra/criptography/bcrypt/bcrypt-adapter.ts
+++ api/src/infra/criptography/bcrypt/bcrypt-adapter.ts
@@ -5,8 +5,6 @@ export class BCryptAdapter implements Hasher {
   private readonly salt = 12
 
   public async hash (value: string): Promise<string> {
-    await bcrypt.hash(value, this.salt)
-
-    return null
+    return await bcrypt.hash(value, this.salt)
   }
 }
