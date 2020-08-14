39b8ed30a9f4be3b94b522e801bec5feeb9a04d5
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-14 20:17:53 -0300

    feat: ensure RequiredFieldValidator not returns if validation succeeds

--- api/src/validation/validators/required-field-validator.ts
+++ api/src/validation/validators/required-field-validator.ts
@@ -4,7 +4,9 @@ import { MissingParamError } from '../../presentation/errors'
 export class RequiredFieldValidator implements Validator {
   public constructor (private readonly field: string) {}
 
-  public validate (_input: any): MissingParamError {
-    return new MissingParamError(this.field)
+  public validate (input: any): MissingParamError {
+    if (!input[this.field]) {
+      return new MissingParamError(this.field)
+    }
   }
 }
