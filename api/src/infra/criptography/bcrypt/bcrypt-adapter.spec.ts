8ebd5c823aa9dd9479b7187c63ee60f01edc6756
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-14 14:24:29 -0300

    test: ensure BCryptAdapter calls bcrypt hash with correct values

---
+++ api/src/infra/criptography/bcrypt/bcrypt-adapter.spec.ts
@@ -0,0 +1,23 @@
+import { BCryptAdapter } from './bcrypt-adapter'
+import bcrypt from 'bcrypt'
+
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return 'hashed_value'
+  }
+}))
+
+const makeSut = (): BCryptAdapter => {
+  return new BCryptAdapter()
+}
+
+describe('BCrypt Adapter', () => {
+  test('Should call bcrypt hash with correct values', async () => {
+    const sut = makeSut()
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+
+    await sut.hash('any_value')
+
+    expect(hashSpy).toHaveBeenCalledWith('any_value', 12)
+  })
+})
