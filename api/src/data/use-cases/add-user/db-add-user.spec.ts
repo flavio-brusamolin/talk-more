d7e68ac05e8750158b652ecb6975fbe8471ea327
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-13 23:26:51 -0300

    test: ensure DbAddUser calls AddUserRepository with correct values

--- api/src/data/use-cases/add-user/db-add-user.spec.ts
+++ api/src/data/use-cases/add-user/db-add-user.spec.ts
@@ -1,11 +1,19 @@
 import { DbAddUser } from './db-add-user'
+import { Hasher, AddUserRepository } from '../../protocols'
 import { AddUserModel } from '../../../domain/use-cases/add-user'
-import { Hasher } from '../../protocols/hasher'
+import { User } from '../../../domain/models/user'
 
 const makeFakeUserData = (): AddUserModel => ({
   name: 'valid_name',
   email: 'valid_email@mail.com',
   password: 'valid_password'
+})
+
+const makeFakeUser = (): User => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'hashed_password'
 })
 
 const makeHasher = (): Hasher => {
@@ -18,17 +26,30 @@ const makeHasher = (): Hasher => {
   return new HasherStub()
 }
 
+const makeAddUserRepository = (): AddUserRepository => {
+  class AddUserRepositoryStub implements AddUserRepository {
+    public async add (_userData: AddUserModel): Promise<User> {
+      return makeFakeUser()
+    }
+  }
+
+  return new AddUserRepositoryStub()
+}
+
 interface SutTypes {
   hasherStub: Hasher
+  addUserRepositoryStub: AddUserRepository
   sut: DbAddUser
 }
 
 const makeSut = (): SutTypes => {
   const hasherStub = makeHasher()
-  const sut = new DbAddUser(hasherStub)
+  const addUserRepositoryStub = makeAddUserRepository()
+  const sut = new DbAddUser(hasherStub, addUserRepositoryStub)
 
   return {
     hasherStub,
+    addUserRepositoryStub,
     sut
   }
 }
@@ -54,5 +75,18 @@ describe('DbAddUser Use Case', () => {
     const promise = sut.add(userData)
 
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should call AddUserRepository with correct values', async () => {
+    const { sut, addUserRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(addUserRepositoryStub, 'add')
+
+    const userData = makeFakeUserData()
+    await sut.add(userData)
+
+    expect(addSpy).toHaveBeenCalledWith({
+      ...userData,
+      password: 'hashed_password'
+    })
   })
 })
