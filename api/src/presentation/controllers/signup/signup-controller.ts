67246a32a82d0315a75a97f6d193383dbec50fc6
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-13 22:28:35 -0300

    feat: ensure SignUpController calls AuthenticateUser with correct values

--- api/src/presentation/controllers/signup/signup-controller.ts
+++ api/src/presentation/controllers/signup/signup-controller.ts
@@ -2,6 +2,7 @@ import { HttpRequest, HttpResponse, Validator } from '../../protocols'
 import { badRequest, serverError, conflict } from '../../helpers/http-helper'
 import { AddUser } from '../../../domain/use-cases/add-user'
 import { DuplicateEmailError } from '../../errors'
+import { AuthenticateUser } from '../../../domain/use-cases/authenticate-user'
 
 export interface SignUpModel {
   name: string
@@ -12,7 +13,8 @@ export interface SignUpModel {
 export class SignUpController {
   public constructor (
     private readonly validator: Validator,
-    private readonly addUser: AddUser
+    private readonly addUser: AddUser,
+    private readonly authenticateUser: AuthenticateUser
   ) {}
 
   public async handle (httpRequest: HttpRequest<SignUpModel>): Promise<HttpResponse> {
@@ -22,7 +24,19 @@ export class SignUpController {
         return badRequest(error)
       }
 
-      await this.addUser.add(httpRequest.body)
+      const { name, email, password } = httpRequest.body
+
+      await this.addUser.add({
+        name,
+        email,
+        password
+      })
+
+      await this.authenticateUser.authenticate({
+        email,
+        password
+      })
+
       return conflict(new DuplicateEmailError())
     } catch (error) {
       return serverError()
