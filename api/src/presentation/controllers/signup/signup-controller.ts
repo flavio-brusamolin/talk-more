7b2114ad7376c554553992d22fac72fd4611aa92
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-13 22:07:51 -0300

    feat: ensure SignUpController returns a conflict error if AddUser returns null

--- api/src/presentation/controllers/signup/signup-controller.ts
+++ api/src/presentation/controllers/signup/signup-controller.ts
@@ -1,6 +1,7 @@
 import { HttpRequest, HttpResponse, Validator } from '../../protocols'
-import { badRequest, serverError } from '../../helpers/http-helper'
+import { badRequest, serverError, conflict } from '../../helpers/http-helper'
 import { AddUser } from '../../../domain/use-cases/add-user'
+import { DuplicateEmailError } from '../../errors'
 
 export interface SignUpModel {
   name: string
@@ -17,10 +18,12 @@ export class SignUpController {
   public async handle (httpRequest: HttpRequest<SignUpModel>): Promise<HttpResponse> {
     try {
       const error = this.validator.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
 
-      this.addUser.add(httpRequest.body)
-
-      return badRequest(error)
+      await this.addUser.add(httpRequest.body)
+      return conflict(new DuplicateEmailError())
     } catch (error) {
       return serverError()
     }
--- api/src/presentation/helpers/http-helper.ts
+++ api/src/presentation/helpers/http-helper.ts
@@ -10,3 +10,8 @@ export const serverError = (): HttpResponse => ({
   statusCode: 500,
   body: new ServerError()
 })
+
+export const conflict = (error: Error): HttpResponse => ({
+  statusCode: 409,
+  body: error
+})
