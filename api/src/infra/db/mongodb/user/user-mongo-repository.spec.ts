20e5153a0f3257ba4575fa22ca525eadbec1753a
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-14 11:47:14 -0300

    test: ensure UserMongoRepository returns a user on loadByEmail success

--- api/src/infra/db/mongodb/user/user-mongo-repository.spec.ts
+++ api/src/infra/db/mongodb/user/user-mongo-repository.spec.ts
@@ -1,10 +1,13 @@
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AddUserModel } from '../../../../domain/use-cases/add-user'
 import { UserMongoRepository } from './user-mongo-repository'
+import { Collection } from 'mongodb'
 
 const makeSut = (): UserMongoRepository => {
   return new UserMongoRepository()
 }
+
+let userCollection: Collection
 
 describe('User Mongo Repository', () => {
   beforeAll(async () => {
@@ -16,8 +19,26 @@ describe('User Mongo Repository', () => {
   })
 
   beforeEach(async () => {
-    const userCollection = await MongoHelper.getCollection('users')
+    userCollection = await MongoHelper.getCollection('users')
     await userCollection.deleteMany({})
+  })
+
+  test('Should return a user on loadByEmail success', async () => {
+    const sut = makeSut()
+
+    await userCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+
+    const user = await sut.loadByEmail('any_email@mail.com')
+
+    expect(user).toBeTruthy()
+    expect(user.id).toBeTruthy()
+    expect(user.name).toBe('any_name')
+    expect(user.email).toBe('any_email@mail.com')
+    expect(user.password).toBe('any_password')
   })
 
   test('Should return a user on add success', async () => {
