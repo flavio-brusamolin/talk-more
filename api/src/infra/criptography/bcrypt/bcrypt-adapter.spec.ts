7bdd5970f31a7b4052e57a019ccd605d75cd30eb
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-14 14:27:44 -0300

    test: ensure BCryptAdapter throws if bcrypt hash throws

--- api/src/infra/criptography/bcrypt/bcrypt-adapter.spec.ts
+++ api/src/infra/criptography/bcrypt/bcrypt-adapter.spec.ts
@@ -20,4 +20,15 @@ describe('BCrypt Adapter', () => {
 
     expect(hashSpy).toHaveBeenCalledWith('any_value', 12)
   })
+
+  test('Should throw if bcrypt hash throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const promise = sut.hash('any_value')
+
+    await expect(promise).rejects.toThrow()
+  })
 })
