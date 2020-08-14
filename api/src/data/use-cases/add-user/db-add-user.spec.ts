12a5c07d12bfe198df72822952a0d169e7758ab7
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-13 23:05:26 -0300

    test: ensure DbAddUser calls Hasher with correct value

---
+++ api/src/data/use-cases/add-user/db-add-user.spec.ts
@@ -0,0 +1,46 @@
+import { DbAddUser } from './db-add-user'
+import { AddUserModel } from '../../../domain/use-cases/add-user'
+import { Hasher } from '../../protocols/hasher'
+
+const makeFakeUserData = (): AddUserModel => ({
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'valid_password'
+})
+
+const makeHasher = (): Hasher => {
+  class HasherStub implements Hasher {
+    public async hash (_value: string): Promise<string> {
+      return 'hashed_password'
+    }
+  }
+
+  return new HasherStub()
+}
+
+interface SutTypes {
+  hasherStub: Hasher
+  sut: DbAddUser
+}
+
+const makeSut = (): SutTypes => {
+  const hasherStub = makeHasher()
+  const sut = new DbAddUser(hasherStub)
+
+  return {
+    hasherStub,
+    sut
+  }
+}
+
+describe('DbAddUser Use Case', () => {
+  test('Should call Hasher with correct value', async () => {
+    const { sut, hasherStub } = makeSut()
+    const hashSpy = jest.spyOn(hasherStub, 'hash')
+
+    const userData = makeFakeUserData()
+    await sut.add(userData)
+
+    expect(hashSpy).toHaveBeenCalledWith(userData.password)
+  })
+})
