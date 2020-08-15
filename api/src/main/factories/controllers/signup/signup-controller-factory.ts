b6b6819e7b63d6864ee47d311160b30c354fbd60
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-15 15:46:39 -0300

    feat: create SignUpController factory

---
+++ api/src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -0,0 +1,8 @@
+import { SignUpController } from '../../../../presentation/controllers/signup/signup-controller'
+import { makeSignUpValidator } from './signup-validator/signup-validator-factory'
+import { makeDbAddUser } from '../../use-cases/add-user/db-add-user-factory'
+import { makeDbAuthenticateUser } from '../../use-cases/authenticate-user/db-authenticate-user'
+
+export const makeSignUpController = (): SignUpController => {
+  return new SignUpController(makeSignUpValidator(), makeDbAddUser(), makeDbAuthenticateUser())
+}
