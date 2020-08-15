6a0431828dad4da87735b3307e45d63d80a74b20
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-15 10:53:47 -0300

    feat: ensure EmailValidatorAdapter calls validator isEmail with correct value

---
+++ api/src/infra/validators/email-validator-adapter.ts
@@ -0,0 +1,10 @@
+import { EmailValidator } from '../../validation/protocols/email-validator'
+import validator from 'validator'
+
+export class EmailValidatorAdapter implements EmailValidator {
+  public isValid (email: string): boolean {
+    validator.isEmail(email)
+
+    return null
+  }
+}
