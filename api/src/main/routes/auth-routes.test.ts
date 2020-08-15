8955865fac9d4abcbf817a7fdc56e3146191b4b3
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-15 19:20:20 -0300

    test: ensure signup route returns 200 on success

---
+++ api/src/main/routes/auth-routes.test.ts
@@ -0,0 +1,31 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+
+describe('Auth Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const userCollection = await MongoHelper.getCollection('users')
+    await userCollection.deleteMany({})
+  })
+
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Test',
+          email: 'test@mail.com',
+          password: '123'
+        })
+        .expect(200)
+    })
+  })
+})
