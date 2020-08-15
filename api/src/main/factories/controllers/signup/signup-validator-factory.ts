028e3908458e83dd9348a615b75815f01fbc4ebd
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-15 15:15:02 -0300

    feat: ensure SignUp Validator Factory calls ValidationComposite with all validations

---
+++ api/src/main/factories/controllers/signup/signup-validator-factory.ts
@@ -0,0 +1,18 @@
+import { Validator } from '../../../../presentation/protocols'
+import { ValidationComposite } from '../../../../validation/validation-composite/validation-composite'
+import { RequiredFieldValidation } from '../../../../validation/validators/required-field/required-field-validation'
+import { EmailValidation } from '../../../../validation/validators/email/email-validation'
+import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator-adapter'
+
+export const makeSignUpValidator = (): ValidationComposite => {
+  const validations: Validator[] = []
+
+  const requiredFields = ['name', 'email', 'password']
+  for (const field of requiredFields) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
+
+  return new ValidationComposite(validations)
+}
