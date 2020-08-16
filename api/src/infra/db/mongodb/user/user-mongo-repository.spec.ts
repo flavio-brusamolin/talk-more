6bdc1835c4df7ce0c8e05db0782714fca050d2d4
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-16 19:14:33 -0300

    test: ensure UserMongoRepository returns a user on loadById success

--- api/src/infra/db/mongodb/user/user-mongo-repository.spec.ts
+++ api/src/infra/db/mongodb/user/user-mongo-repository.spec.ts
@@ -64,4 +64,23 @@ describe('User Mongo Repository', () => {
     expect(user.email).toBe(userData.email)
     expect(user.password).toBe(userData.password)
   })
+
+  test('Should return a user on loadById success', async () => {
+    const sut = makeSut()
+
+    const result = await userCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    const [userRecord] = result.ops
+
+    const user = await sut.loadById(userRecord._id)
+
+    expect(user).toBeTruthy()
+    expect(user.id).toEqual(userRecord._id)
+    expect(user.name).toBe('any_name')
+    expect(user.email).toBe('any_email@mail.com')
+    expect(user.password).toBe('any_password')
+  })
 })
