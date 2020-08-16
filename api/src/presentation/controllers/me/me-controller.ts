6ec458091668e179a3cd01fcc9ba437d606ada6a
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-16 18:03:45 -0300

    feat: ensure MeController returns an ok response on success

--- api/src/presentation/controllers/me/me-controller.ts
+++ api/src/presentation/controllers/me/me-controller.ts
@@ -1,16 +1,14 @@
 import { Controller, HttpResponse, HttpRequest } from '../../protocols'
 import { LoadUserById } from '../../../domain/use-cases/load-user-by-id'
-import { serverError } from '../../helpers/http-helper'
+import { serverError, ok } from '../../helpers/http-helper'
 
 export class MeController implements Controller {
   public constructor (private readonly loadUserById: LoadUserById) {}
 
   public async handle (httpRequest: HttpRequest<any>): Promise<HttpResponse> {
     try {
-      const { userId } = httpRequest
-      await this.loadUserById.loadById(userId)
-
-      return null
+      const user = await this.loadUserById.loadById(httpRequest.userId)
+      return ok(user)
     } catch (error) {
       console.error(error)
       return serverError()
