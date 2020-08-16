18635bf077823832da275873854afac7ffeba262
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-16 17:48:11 -0300

    test: ensure MeController calls LoadUserById with correct value

---
+++ api/src/presentation/controllers/me/me-controller.spec.ts
@@ -0,0 +1,52 @@
+import { MeController } from './me-controller'
+import { User } from '../../../domain/models/user'
+import { LoadUserById } from '../../../domain/use-cases/load-user-by-id'
+import { HttpRequest } from '../../protocols'
+
+const makeFakeRequest = (): HttpRequest<any> => ({
+  userId: 'any_id'
+})
+
+const makeFakeUser = (): User => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password'
+})
+
+const makeLoadUserById = (): LoadUserById => {
+  class LoadUserByIdStub implements LoadUserById {
+    public async loadById (_id: string): Promise<User> {
+      return makeFakeUser()
+    }
+  }
+
+  return new LoadUserByIdStub()
+}
+
+interface SutTypes {
+  loadUserByIdStub: LoadUserById
+  sut: MeController
+}
+
+const makeSut = (): SutTypes => {
+  const loadUserByIdStub = makeLoadUserById()
+  const sut = new MeController(loadUserByIdStub)
+
+  return {
+    loadUserByIdStub,
+    sut
+  }
+}
+
+describe('Me Controller', () => {
+  test('Should call LoadUserById with correct value', async () => {
+    const { sut, loadUserByIdStub } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadUserByIdStub, 'loadById')
+
+    const httpRequest = makeFakeRequest()
+    await sut.handle(httpRequest)
+
+    expect(loadByIdSpy).toHaveBeenCalledWith(httpRequest.userId)
+  })
+})
