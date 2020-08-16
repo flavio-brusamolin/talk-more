adb245bef78de411d0cdfd898f8c7b1817395e23
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-16 09:47:23 -0300

    feat: ensure SignInController calls AuthenticateUser with correct values

--- api/src/presentation/controllers/signin/signin-controller.ts
+++ api/src/presentation/controllers/signin/signin-controller.ts
@@ -1,5 +1,6 @@
 import { Controller, HttpResponse, HttpRequest, Validator } from '../../protocols'
 import { badRequest } from '../../helpers/http-helper'
+import { AuthenticateUser } from '../../../domain/use-cases/authenticate-user'
 
 export interface SignInModel {
   email: string
@@ -7,10 +8,20 @@ export interface SignInModel {
 }
 
 export class SignInController implements Controller {
-  public constructor (private readonly validator: Validator) {}
+  public constructor (
+    private readonly validator: Validator,
+    private readonly authenticateUser: AuthenticateUser
+  ) {}
 
   public async handle (httpRequest: HttpRequest<SignInModel>): Promise<HttpResponse> {
     const error = this.validator.validate(httpRequest.body)
+
+    const { email, password } = httpRequest.body
+
+    await this.authenticateUser.authenticate({
+      email,
+      password
+    })
 
     return badRequest(error)
   }
