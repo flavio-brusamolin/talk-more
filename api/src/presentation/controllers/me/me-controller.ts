f35b2e5136e3a433ff0c19043dfa786d7c5b002c
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-16 17:51:25 -0300

    feat: ensure MeController returns an internal server error if LoadUserById throws

--- api/src/presentation/controllers/me/me-controller.ts
+++ api/src/presentation/controllers/me/me-controller.ts
@@ -1,13 +1,19 @@
 import { Controller, HttpResponse, HttpRequest } from '../../protocols'
 import { LoadUserById } from '../../../domain/use-cases/load-user-by-id'
+import { serverError } from '../../helpers/http-helper'
 
 export class MeController implements Controller {
   public constructor (private readonly loadUserById: LoadUserById) {}
 
   public async handle (httpRequest: HttpRequest<any>): Promise<HttpResponse> {
-    const { userId } = httpRequest
-    await this.loadUserById.loadById(userId)
+    try {
+      const { userId } = httpRequest
+      await this.loadUserById.loadById(userId)
 
-    return null
+      return null
+    } catch (error) {
+      console.error(error)
+      return serverError()
+    }
   }
 }
