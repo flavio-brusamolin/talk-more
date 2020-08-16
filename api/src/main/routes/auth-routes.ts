1005fe01d3950f9114e2337c3eb3136b848e1ff9
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-16 10:33:22 -0300

    feat: ensure signin route returns 200 on signin

--- api/src/main/routes/auth-routes.ts
+++ api/src/main/routes/auth-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeSignUpController } from '../factories/controllers/signup/signup-controller-factory'
+import { makeSignInController } from '../factories/controllers/signin/signin-controller-factory'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
+  router.post('/signin', adaptRoute(makeSignInController()))
 }
