23d0415beb41013fcbfca7a4826ba4d8eee1e779
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-16 13:23:24 -0300

    feat: create core module

---
+++ web-app/src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core'
+
+import { throwIfAlreadyLoaded } from './guards/module-import.guard'
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [],
+  providers: []
+})
+export class CoreModule {
+  constructor (@Optional() @SkipSelf() parentModule: CoreModule) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule')
+  }
+}
