f3eadaa15258854e2578771729dcdabac9a11c23
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-16 09:50:17 -0300

    feat: ensure SignInController returns an internal server error if AuthenticateUser throws

--- api/src/presentation/controllers/signin/signin-controller.ts
+++ api/src/presentation/controllers/signin/signin-controller.ts
@@ -1,5 +1,5 @@
 import { Controller, HttpResponse, HttpRequest, Validator } from '../../protocols'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, serverError } from '../../helpers/http-helper'
 import { AuthenticateUser } from '../../../domain/use-cases/authenticate-user'
 
 export interface SignInModel {
@@ -14,15 +14,20 @@ export class SignInController implements Controller {
   ) {}
 
   public async handle (httpRequest: HttpRequest<SignInModel>): Promise<HttpResponse> {
-    const error = this.validator.validate(httpRequest.body)
+    try {
+      const error = this.validator.validate(httpRequest.body)
 
-    const { email, password } = httpRequest.body
+      const { email, password } = httpRequest.body
 
-    await this.authenticateUser.authenticate({
-      email,
-      password
-    })
+      await this.authenticateUser.authenticate({
+        email,
+        password
+      })
 
-    return badRequest(error)
+      return badRequest(error)
+    } catch (error) {
+      console.error(error)
+      return serverError()
+    }
   }
 }
