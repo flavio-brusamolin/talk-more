29c73e94d62a96ce6fed9949371341a44ebb60cc
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-16 18:16:58 -0300

    feat: ensure MeController returns an forbidden error if LoadUserById returns null

--- api/src/presentation/controllers/me/me-controller.ts
+++ api/src/presentation/controllers/me/me-controller.ts
@@ -1,13 +1,20 @@
 import { Controller, HttpResponse, HttpRequest } from '../../protocols'
 import { LoadUserById } from '../../../domain/use-cases/load-user-by-id'
-import { serverError, ok } from '../../helpers/http-helper'
+import { serverError, ok, forbidden } from '../../helpers/http-helper'
+import { InvalidParamError } from '../../errors'
 
 export class MeController implements Controller {
   public constructor (private readonly loadUserById: LoadUserById) {}
 
   public async handle (httpRequest: HttpRequest<any>): Promise<HttpResponse> {
     try {
-      const user = await this.loadUserById.loadById(httpRequest.userId)
+      const { userId } = httpRequest
+
+      const user = await this.loadUserById.loadById(userId)
+      if (!user) {
+        return forbidden(new InvalidParamError('userId'))
+      }
+
       return ok(user)
     } catch (error) {
       console.error(error)
