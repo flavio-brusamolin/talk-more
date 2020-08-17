a581fa3b466a157be67865613d657cfa2ed36725
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-17 20:16:17 -0300

    feat: ensure AuthMiddleware returns an internal server error if CheckUserAuthentication throws

--- api/src/presentation/middlewares/auth-middleware.ts
+++ api/src/presentation/middlewares/auth-middleware.ts
@@ -1,14 +1,20 @@
 import { Middleware } from '../protocols/middleware'
 import { HttpRequest, HttpResponse } from '../protocols'
 import { CheckUserAuthentication } from '../../domain/use-cases/check-user-authentication'
+import { serverError } from '../helpers/http-helper'
 
 export class AuthMiddleware implements Middleware {
   public constructor (private readonly checkUserAuthentication: CheckUserAuthentication) {}
 
   public async handle (httpRequest: HttpRequest<any>): Promise<HttpResponse> {
-    const accessToken = httpRequest.headers?.authorization
-    await this.checkUserAuthentication.checkAuthentication(accessToken)
+    try {
+      const accessToken = httpRequest.headers?.authorization
+      await this.checkUserAuthentication.checkAuthentication(accessToken)
 
-    return null
+      return null
+    } catch (error) {
+      console.error(error)
+      return serverError()
+    }
   }
 }
