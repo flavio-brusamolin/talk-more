d468ff0238630bc94e6c48403d94672fb766f3ac
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-16 22:01:35 -0300

    feat: ensure LoadPlansController returns an internal server error if LoadPlans throws

--- api/src/presentation/controllers/load-plans/load-plans-controller.ts
+++ api/src/presentation/controllers/load-plans/load-plans-controller.ts
@@ -1,12 +1,18 @@
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { LoadPlans } from '../../../domain/use-cases/load-plans'
+import { serverError } from '../../helpers/http-helper'
 
 export class LoadPlansController implements Controller {
   public constructor (private readonly loadPlans: LoadPlans) {}
 
   public async handle (_httpRequest: HttpRequest<any>): Promise<HttpResponse> {
-    await this.loadPlans.load()
+    try {
+      await this.loadPlans.load()
 
-    return null
+      return null
+    } catch (error) {
+      console.error(error)
+      return serverError()
+    }
   }
 }
