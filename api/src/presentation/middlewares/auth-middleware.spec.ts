c7d86412685105b00953b1f5cd302c3375f0fdbd
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-17 20:16:25 -0300

    test: ensure AuthMiddleware returns an internal server error if CheckUserAuthentication throws

--- api/src/presentation/middlewares/auth-middleware.spec.ts
+++ api/src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,6 +1,7 @@
 import { AuthMiddleware } from './auth-middleware'
 import { HttpRequest } from '../protocols'
 import { CheckUserAuthentication } from '../../domain/use-cases/check-user-authentication'
+import { serverError } from '../helpers/http-helper'
 
 const makeFakeRequest = (): HttpRequest<any> => ({
   headers: {
@@ -42,5 +43,17 @@ describe('Auth Middleware', () => {
     await sut.handle(httpRequest)
 
     expect(checkAuthenticationSpy).toHaveBeenCalledWith(httpRequest.headers.authorization)
+  })
+
+  test('Should return an internal server error if CheckUserAuthentication throws', async () => {
+    const { sut, checkUserAuthenticationStub } = makeSut()
+    jest.spyOn(checkUserAuthenticationStub, 'checkAuthentication').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const httpRequest = makeFakeRequest()
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual(serverError())
   })
 })
