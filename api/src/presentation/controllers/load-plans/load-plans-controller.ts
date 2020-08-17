3e33e711d523acb6092afd78ad7242a74abdf07a
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-16 22:05:31 -0300

    feat: ensure LoadPlansController returns an ok response on success

--- api/src/presentation/controllers/load-plans/load-plans-controller.ts
+++ api/src/presentation/controllers/load-plans/load-plans-controller.ts
@@ -1,15 +1,14 @@
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { LoadPlans } from '../../../domain/use-cases/load-plans'
-import { serverError } from '../../helpers/http-helper'
+import { serverError, ok } from '../../helpers/http-helper'
 
 export class LoadPlansController implements Controller {
   public constructor (private readonly loadPlans: LoadPlans) {}
 
   public async handle (_httpRequest: HttpRequest<any>): Promise<HttpResponse> {
     try {
-      await this.loadPlans.load()
-
-      return null
+      const plans = await this.loadPlans.load()
+      return ok(plans)
     } catch (error) {
       console.error(error)
       return serverError()
