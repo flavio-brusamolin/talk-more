ba7243722a54005bf93a5f6ca5728361c7be678b
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-17 20:29:21 -0300

    feat: ensure AuthMiddleware returns an ok response on success

--- api/src/presentation/middlewares/auth-middleware.ts
+++ api/src/presentation/middlewares/auth-middleware.ts
@@ -1,7 +1,7 @@
 import { Middleware } from '../protocols/middleware'
 import { HttpRequest, HttpResponse } from '../protocols'
 import { CheckUserAuthentication } from '../../domain/use-cases/check-user-authentication'
-import { serverError, unauthorized } from '../helpers/http-helper'
+import { serverError, unauthorized, ok } from '../helpers/http-helper'
 
 export class AuthMiddleware implements Middleware {
   public constructor (private readonly checkUserAuthentication: CheckUserAuthentication) {}
@@ -9,9 +9,13 @@ export class AuthMiddleware implements Middleware {
   public async handle (httpRequest: HttpRequest<any>): Promise<HttpResponse> {
     try {
       const accessToken = httpRequest.headers?.authorization
-      await this.checkUserAuthentication.checkAuthentication(accessToken)
 
-      return unauthorized()
+      const userId = await this.checkUserAuthentication.checkAuthentication(accessToken)
+      if (!userId) {
+        return unauthorized()
+      }
+
+      return ok({ userId })
     } catch (error) {
       console.error(error)
       return serverError()
