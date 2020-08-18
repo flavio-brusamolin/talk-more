7d7b59982448aedcc90dcce6a735d027eb672877
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-18 10:22:50 -0300

    refactor: move loadLoggedUser method to AuthService

--- web-app/src/app/core/services/auth.service.ts
+++ web-app/src/app/core/services/auth.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs'
 import { tap } from 'rxjs/operators'
 
 import { environment } from 'src/environments/environment'
-import { SignUpData, SignInData, Token } from 'src/app/data/models'
+import { SignUpData, SignInData, Token, User } from 'src/app/data/models'
 
 @Injectable()
 export class AuthService {
@@ -23,6 +23,10 @@ export class AuthService {
     return this.http
       .post<Token>(`${this.url}/signup`, userData)
       .pipe(tap(({ accessToken }) => this.setToken(accessToken)))
+  }
+
+  loadLoggedUser (): Observable<User> {
+    return this.http.get<User>(`${this.url}/me`)
   }
 
   getToken (): string {
--- web-app/src/app/data/data.module.ts
+++ web-app/src/app/data/data.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core'
 
 import { PlanService } from './services/plan.service'
-import { UserService } from './services/user.service'
 
 @NgModule({
   declarations: [],
   imports: [],
   providers: [
-    PlanService,
-    UserService
+    PlanService
   ]
 })
 export class DataModule { }
