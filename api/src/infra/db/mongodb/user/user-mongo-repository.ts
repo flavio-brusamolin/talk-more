88abb40a29ef3e4c5a43667754f0a76726c07ca1
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-18 01:26:57 -0300

    feat: ensure UserMongoRepository updates the user plan on updatePlan success

--- api/src/infra/db/mongodb/user/user-mongo-repository.ts
+++ api/src/infra/db/mongodb/user/user-mongo-repository.ts
@@ -1,10 +1,11 @@
-import { AddUserRepository, LoadUserByEmailRepository, LoadUserByIdRepository } from '../../../../data/protocols'
+import { AddUserRepository, LoadUserByEmailRepository, LoadUserByIdRepository, UpdateUserPlanRepository } from '../../../../data/protocols'
 import { AddUserModel } from '../../../../domain/use-cases/add-user'
 import { User } from '../../../../domain/models/user'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { ObjectId } from 'mongodb'
+import { SubscribePlanModel } from '../../../../domain/use-cases/subscribe-plan'
 
-export class UserMongoRepository implements AddUserRepository, LoadUserByEmailRepository, LoadUserByIdRepository {
+export class UserMongoRepository implements AddUserRepository, LoadUserByEmailRepository, LoadUserByIdRepository, UpdateUserPlanRepository {
   public async loadByEmail (email: string): Promise<User> {
     const userCollection = await MongoHelper.getCollection('users')
     const userRecord = await userCollection.findOne({ email })
@@ -24,5 +25,17 @@ export class UserMongoRepository implements AddUserRepository, LoadUserByEmailRe
     const userCollection = await MongoHelper.getCollection('users')
     const userRecord = await userCollection.findOne({ _id: new ObjectId(id) })
     return userRecord && MongoHelper.map(userRecord)
+  }
+
+  public async updatePlan ({ userId, planId }: SubscribePlanModel): Promise<User> {
+    const userCollection = await MongoHelper.getCollection('users')
+
+    const userRecord = await userCollection.findOneAndUpdate(
+      { _id: userId },
+      { $set: { planId: planId } },
+      { returnOriginal: false }
+    )
+
+    return MongoHelper.map(userRecord.value)
   }
 }
