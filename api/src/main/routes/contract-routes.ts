11fcb109845b3d91cbcdb78fba7fd143de5c01d8
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-18 02:11:23 -0300

    feat: ensure contract route returns 200 on create contract with valid accessToken

--- api/src/main/routes/contract-routes.ts
+++ api/src/main/routes/contract-routes.ts
@@ -4,5 +4,5 @@ import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeCreateContractController } from '../factories/controllers/create-contract/create-contract-controller-factory'
 
 export default (router: Router): void => {
-  router.post('/contract', auth, adaptRoute(makeCreateContractController()))
+  router.post('/contracts', auth, adaptRoute(makeCreateContractController()))
 }
