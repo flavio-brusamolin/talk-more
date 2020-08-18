94263dd97c88b62d869cfd8d14a51e75a9c4e55e
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-18 01:08:23 -0300

    feat: ensure PlanMongoRepository returns a plan on loadById success

--- api/src/infra/db/mongodb/plan/plan-mongo-repository.ts
+++ api/src/infra/db/mongodb/plan/plan-mongo-repository.ts
@@ -1,11 +1,18 @@
-import { LoadPlansRepository } from '../../../../data/protocols'
+import { LoadPlansRepository, LoadPlanByIdRepository } from '../../../../data/protocols'
 import { Plan } from '../../../../domain/models/plan'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { ObjectId } from 'mongodb'
 
-export class PlanMongoRepository implements LoadPlansRepository {
+export class PlanMongoRepository implements LoadPlansRepository, LoadPlanByIdRepository {
   public async loadAll (): Promise<Plan[]> {
     const planCollection = await MongoHelper.getCollection('plans')
     const planRecords = await planCollection.find().toArray()
     return MongoHelper.mapArray(planRecords)
+  }
+
+  public async loadById (id: string): Promise<Plan> {
+    const planCollection = await MongoHelper.getCollection('plans')
+    const planRecord = await planCollection.findOne({ _id: new ObjectId(id) })
+    return MongoHelper.map(planRecord)
   }
 }
