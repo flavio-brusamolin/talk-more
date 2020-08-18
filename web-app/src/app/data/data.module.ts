630da9b95d97d932e2d48053f0eabda08bf1324d
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-18 10:01:11 -0300

    feat: create UserService

--- web-app/src/app/data/data.module.ts
+++ web-app/src/app/data/data.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core'
 
 import { PlanService } from './services/plan.service'
+import { UserService } from './services/user.service'
 
 @NgModule({
   declarations: [],
   imports: [],
   providers: [
-    PlanService
+    PlanService,
+    UserService
   ]
 })
 export class DataModule { }
