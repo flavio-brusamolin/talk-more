e723e46107f2962e676f82c0499f706c47bb2fa7
Flavio Brusamolin <flaviobrusamolin@gec.inatel.br>
2020-08-17 21:22:25 -0300

    test: ensure JwtAdapter throws if jwt verify throws

--- api/src/infra/criptography/jwt/jwt-adapter.spec.ts
+++ api/src/infra/criptography/jwt/jwt-adapter.spec.ts
@@ -58,4 +58,15 @@ describe('Jwt Adapter', () => {
 
     expect(verifySpy).toHaveBeenCalledWith('any_token', secret)
   })
+
+  test('Should throw if jwt verify throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const promise = sut.decrypt('any_token')
+
+    await expect(promise).rejects.toThrow()
+  })
 })
